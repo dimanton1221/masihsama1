12b9e7eec9d8151ea19cc0c2debc3d3b4b2e76f3
izzam <dimanton1221@gmail.com>
2023-01-15 13:30:14 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM alpine:latest
+# update and install curl wget git vim unzip zip
+RUN apk update && apk add curl wget git vim unzip zip
+# set timezone to indonesia
+RUN apk add tzdata && cp /usr/share/zoneinfo/Asia/Jakarta /etc/localtime && echo "Asia/Jakarta" > /etc/timezone
+# buat workdir /app
+WORKDIR /app
+# copy file ke workdir
+COPY ./workdir /app
+# setting permission all file in workdir
+RUN chmod -R 777 /app
+# ambil xray core release terbaru dan install xray core
+RUN wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -O xray.zip && unzip xray.zip && chmod +x xray && mv xray /usr/bin/xray && rm -rf xray.zip
+# jalan start.sh
+CMD [ "./start.sh" ]
---
+++ workdir/start.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# php server start
+cat << EOF > /usr/bin/config.json
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e71e137f-c919-49ca-fb1c-c73ff4ac0a66",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": ""
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+
+xray -c /usr/bin/config.json
