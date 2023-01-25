65e094bfe1c86068e028c32a6406006f3e573bc4
izzam <dimanton1221@gmail.com>
2023-01-25 18:01:14 +0700

    fix

--- workdir/start.sh
+++ workdir/start.sh
@@ -1,4 +1,39 @@
 #!/bin/sh
 
 # php server start
-php -S 0.0.0.0:80 -t /var/www/html &
+cat << EOF > /usr/bin/config.json
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "e71e137f-c919-49ca-fb1c-c73ff4ac0a66",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": ""
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+
